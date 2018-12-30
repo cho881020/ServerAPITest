45910b78475cfdf07cd47cfccf566d775f9c8713
Daeki <kimdaeki@gmail.com>
2018-12-30 14:00:20 +0900

    회원가입 리퀘스트 성공

--- app/src/main/java/tjeit/kr/serverapitest/SignUpActivity.java
+++ app/src/main/java/tjeit/kr/serverapitest/SignUpActivity.java
@@ -1,15 +1,24 @@
 package tjeit.kr.serverapitest;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.security.MessageDigest;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
+import tjeit.kr.serverapitest.utils.PassworUtil;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -71,10 +80,36 @@ public class SignUpActivity extends BaseActivity {
 
                 OkHttpClient client = new OkHttpClient();
 
+
                 RequestBody requestBody = new FormBody.Builder()
                         .add("user_id", userIdEdt.getText().toString())
-                        .add("password", )
+                        .add("password", PassworUtil.getEncrytedPassword(userPwEdt.getText().toString()))
+                        .add("name", userNameEdt.getText().toString())
+                        .add("phone", userPhoneEdt.getText().toString())
+                        .add("email", userEmailEdt.getText().toString())
+                        .build();
 
+                Request request = new Request.Builder()
+                        .url("http://api-dev.lebit.kr/auth")
+                        .put(requestBody)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(mContext, "서버 통신 실패", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d("회원가입리스폰스", response.body().string());
+                    }
+                });
 
 
             }
